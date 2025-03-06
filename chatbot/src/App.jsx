fb9c0837870f7ea5ef0ede0945d4ba0cfea8523f
Gustavo Rodrigues <gustavocr.developer@gmail.com>
2025-03-06 16:02:15 +0000

    aula 9 chatbot

--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -1,130 +1,121 @@
-import { useState, useEffect } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
 import { auth, db } from "./firebase";
-import {
-  collection,
-  addDoc,
-  query,
-  orderBy,
-  onSnapshot,
-  serverTimestamp,
-} from "firebase/firestore";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { collection, query, onSnapshot } from "firebase/firestore";
 
-const initialState = [
-  {
-    id: 1,
-    displayName: "Gustavo",
-    text: "blablabla",
-  },
-];
+const Title = styled.h1`
+  font-size: 1.5rem;
+  text-align: center;
+  color: #4368bf;
+  margin-bottom: 2rem;
+`;
 
-function Auth({ setUser }) {
+const SubTitle = styled.h2`
+  font-size: 1.2rem;
+  text-align: center;
+  color: #4368bf;
+`;
+
+const Wrapper = styled.section`
+  padding: 4rem;
+`;
+
+const Form = styled.form`
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  max-width: 300px;
+`;
+
+const FormInput = styled.input`
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: 1px solid #ccc;
+`;
+
+const FormButton = styled.button`
+  padding: 0.5rem;
+  font-size: 1rem;
+  background-color: #4368bf;
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
+
+function App() {
   const [formValues, setFormValues] = useState({
     email: "",
     password: "",
   });
-
-  const handleInput = (evt, name) => {
-    setFormValues({
-      ...formValues,
-      [name]: evt.target.value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    try {
-      e.preventDefault();
-      const response = await signInWithEmailAndPassword(
-        auth,
-        formValues.email,
-        formValues.password
-      );
-
-      setUser(response.user);
-      setFormValues({
-        email: "",
-        password: "",
-      });
-    } catch {
-      alert("Usuário ou senha invalidos");
-    }
-  };
-
-  return (
-    <div style={{ marginBottom: "5rem" }}>
-      <h2>Faça login para acessar o chat</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={formValues.email}
-          onChange={(e) => handleInput(e, "email")}
-          placeholder="Digite sua mensagem..."
-        />
-        <input
-          type="text"
-          value={formValues.password}
-          onChange={(e) => handleInput(e, "password")}
-          placeholder="Digite sua senha..."
-        />
-        <button type="submit">Entrar</button>
-        {JSON.stringify(formValues)}
-      </form>
-    </div>
-  );
-}
-
-function App() {
-  const [messages, setMessages] = useState(initialState);
-  const [newMessage, setNewMessage] = useState("");
   const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const q = query(collection(db, "messages"), orderBy("createdAt"));
-    const unsubscribe = onSnapshot(q, (snapshot) => {
-      setMessages(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-    });
-    return () => unsubscribe();
-  }, []);
-
+  const [messages, setMessages] = useState([]);
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    if (newMessage.trim() === "") return;
-
-    await addDoc(collection(db, "messages"), {
-      text: newMessage,
-      createdAt: serverTimestamp(),
-      uid: auth.currentUser.uid,
-      displayName: auth.currentUser.displayName,
-    });
-    setNewMessage("");
+    const response = await signInWithEmailAndPassword(
+      auth,
+      formValues.email,
+      formValues.password
+    );
+    setUser(response.user);
   };
-  return (
-    <div className="chat-container">
-      <Auth setUser={setUser} />
 
-      {user ? (
-        <>
-          <div className="messages">
-            {messages.map((msg) => (
-              <div key={msg.id}>
-                <strong>{msg.displayName}: </strong>
-                {msg.text}
-              </div>
+  useEffect(() => {
+    if (user) {
+      const q = query(collection(db, "messages"));
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        const dbMessages = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setMessages(dbMessages);
+      });
+      return () => unsubscribe();
+    }
+  }, [user]);
+
+  return (
+    <Wrapper>
+      <Title>Chatbot</Title>
+      {!user ? (
+        <Form onSubmit={handleSubmit}>
+          <SubTitle>Login</SubTitle>
+          <FormInput
+            type="text"
+            placeholder="E-mail"
+            onChange={(e) =>
+              setFormValues({
+                ...formValues,
+                email: e.target.value,
+              })
+            }
+          />
+          <FormInput
+            type="text"
+            placeholder="Senha"
+            onChange={(e) => {
+              setFormValues({
+                ...formValues,
+                password: e.target.value,
+              });
+            }}
+          />
+          <FormButton type="submit">Enviar</FormButton>
+        </Form>
+      ) : (
+        <div>
+          <SubTitle>Bem vindo ao chat {user.email}</SubTitle>
+          <div>
+            {messages.map((message, key) => (
+              <p key={`message-${message.id}-${key}`}>{message.text}</p>
             ))}
           </div>
-          <form className="message-form" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={newMessage}
-              onChange={(e) => setNewMessage(e.target.value)}
-              placeholder="Digite sua mensagem..."
-            />
-            <button type="submit">Enviar</button>
-          </form>
-        </>
-      ) : null}
-    </div>
+        </div>
+      )}
+    </Wrapper>
   );
 }
 
